a0cab24dc40e62ebb4e1c39598816c26e8454ef8
Késia Silva <kesiasilvab6@gmail.com>
2023-11-16 18:52:37 -0300

    Update calculadora-switch.c

--- calculadora-switch.c
+++ calculadora-switch.c
@@ -5,38 +5,39 @@ int main(){
 	double valor1, valor2, soma, subt, mult, divi;
 	int operador;
 	
-	printf("calculadora simples\n");
-	printf("digite dois numeros para calcular: ");
-	scanf("%lf %lf", &valor1, &valor2);
+	printf("   _-_-_-_-calculadora_-_-_-_\n");
+	printf("Digite dois numeros para calcular:\n");
+	scanf("%lf", &valor1);
+	scanf("%lf", &valor2);
 	
-	printf("escolha 1 para: +;  2 para: -; 3 para: *; 4 para: /\n");
+	printf("\nEscolha:\n 1 para: +;\n 2 para: -;\n 3 para: *;\n 4 para: /;\n");
 	scanf("%i", &operador);
 	
 	
 	switch(operador){
 		case 1:
 		soma = valor1 + valor2;
-			printf("a soma = %.2lf", soma);
+			printf("\na soma = %.2lf", soma);
 			break;
 			
 		case 2:
 		subt = valor1 - valor2;
-		    printf("a subtracao = %.2lf", subt);	
+		    printf("\na subtracao = %.2lf", subt);	
 		    break;
 		    
 		case 3:
 		mult = valor1 * valor2;
-		    printf("a multiplicacao = %.2lf", mult);	
+		    printf("\na multiplicacao = %.2lf", mult);	
 		    break;
 		    
 	    case 4:
 	    	if(valor2 > 0){
 	    	divi = valor1 / valor2;
-		    printf("a divisao = %.2lf", divi);	
+		    printf("\na divisao = %.2lf", divi);	
          	}
 		    
 		    else{
-		    	printf("nao existe divisao por zero");
+		    	printf("\nnao existe divisao por zero");
 			}
 			break;
 		
